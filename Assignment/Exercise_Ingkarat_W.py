f29cbe4588d97f35da3c14521834d08785f5b656
ohmikr <ingkarat.watt@gmail.com>
2020-04-17 18:12:52 +0700

    README

---
+++ Assignment/Exercise_Ingkarat_W.py
@@ -0,0 +1,10 @@
+FoundEng = "Foundation English:"
+GenBus = "General Business:"
+IntroCom = "Introduction to Computer Systems:"
+ComPro = "Computer Programming:"
+
+print("---Your Score---")
+print(FoundEng, 86.35)
+print(GenBus, 75)
+print(IntroCom, 25.34)
+print(ComPro,99.99)
